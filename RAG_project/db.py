01145589b49ea4ac284ed6c8453b1413ebf0d203
Aditya Mishra <aditya.datateam@rentomojo.com>
2026-06-15 23:59:22 +0530

    learnt about RAG vector DB's and how to load chunks to vector DB

---
+++ RAG_project/db.py
@@ -0,0 +1,57 @@
+# from langchain_community.vectorstores import Chroma
+# from langchain_openai import OpenAIEmbeddings
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# from langchain_core.documents import Document
+
+# embedding_model = OpenAIEmbeddings()
+
+# docs=[]
+
+# vectorstore = Chroma.from_documents(
+#     documents=docs,
+#     embedding=embedding_model,
+#     persist_directory="chroma_db",
+        
+# )
+
+# vectorstore.similarity_search("query",k=2)
+
+# load the pdf 
+# split to chunks
+# create embeddings for each chunk
+# store in chromadb
+
+
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_mistralai import ChatMistralAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# lead the pdf:
+data = PyPDFLoader("./quantum_mechanics_notes.pdf")
+docs=data.load()
+
+splitter=RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=100
+)
+
+# split to chunks
+chunks=splitter.split_documents(docs)
+
+# create the embeddings for each chunk and store in chromadb
+embedding_model=OpenAIEmbeddings()
+
+# creating a db 
+vectorstore=Chroma.from_documents(
+    documents=chunks,
+    embedding=embedding_model,
+    persist_directory="chroma_db",
+)
+
--- RAG_project/main.py
+++ RAG_project/main.py
@@ -1,4 +1,5 @@
 ### we use the recursivetext splitter to split the text into smaller chunks and then we can use the chunks to train our model.
+from future import annotations
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter  
 from langchain_core.prompts import ChatPromptTemplate
@@ -6,28 +7,9 @@ from pydantic import BaseModel
 from dotenv import load_dotenv
 load_dotenv()
 
-# loading the pdf 
-
-data =PyPDFLoader("./quantum_mechanics_notes.pdf")
-docs=data.load()
-
-# spliting the document
-splitter=RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=100
-    )
-
-#chunking the document into smaller chunks, and feeding it to AI models
-chunks = splitter.split_documents(docs)
-
-
-
-
-
-
-
-
-
-
-
-
-
+template=ChatPromptTemplate.from_messages([
+    
+    ("system","you are a AI that will summarise the text and answer the question based on the text. "    ),
+    ("human","{data}")
+    
+])
