d4f2c8d829598e1ebbcca1815cf24123ceb65776
Aditya Mishra <aditya2vibha@gmail.com>
2026-06-18 21:30:46 +0530

    top 2 most similar search engine

--- RAG_project/db.py
+++ RAG_project/db.py
@@ -1,23 +1,23 @@
-# from langchain_community.vectorstores import Chroma
-# from langchain_openai import OpenAIEmbeddings
-# from dotenv import load_dotenv
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
-# from langchain_core.documents import Document
+from langchain_core.documents import Document
 
-# embedding_model = OpenAIEmbeddings()
+embedding_model = OpenAIEmbeddings()
 
-# docs=[]
+docs=[]
 
-# vectorstore = Chroma.from_documents(
-#     documents=docs,
-#     embedding=embedding_model,
-#     persist_directory="chroma_db",
+vectorstore = Chroma.from_documents(
+    documents=docs,
+    embedding=embedding_model,
+    persist_directory="chroma_db",
         
-# )
+)
 
-# vectorstore.similarity_search("query",k=2)
+vectorstore.similarity_search("query",k=2)
 
 # load the pdf 
 # split to chunks
@@ -25,33 +25,33 @@
 # store in chromadb
 
 
-from langchain_community.vectorstores import Chroma
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
-from langchain_mistralai import ChatMistralAI
-from dotenv import load_dotenv
+# from langchain_community.vectorstores import Chroma
+# from langchain_community.document_loaders import PyPDFLoader
+# from langchain_text_splitters import RecursiveCharacterTextSplitter
+# from langchain_openai import OpenAIEmbeddings
+# from langchain_mistralai import ChatMistralAI
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-# lead the pdf:
-data = PyPDFLoader("./quantum_mechanics_notes.pdf")
-docs=data.load()
+# # lead the pdf:
+# data = PyPDFLoader("./quantum_mechanics_notes.pdf")
+# docs=data.load()
 
-splitter=RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=100
-)
+# splitter=RecursiveCharacterTextSplitter(
+#     chunk_size=1000, chunk_overlap=100
+# )
 
-# split to chunks
-chunks=splitter.split_documents(docs)
+# # split to chunks
+# chunks=splitter.split_documents(docs)
 
-# create the embeddings for each chunk and store in chromadb
-embedding_model=OpenAIEmbeddings()
+# # create the embeddings for each chunk and store in chromadb
+# embedding_model=OpenAIEmbeddings()
 
-# creating a db 
-vectorstore=Chroma.from_documents(
-    documents=chunks,
-    embedding=embedding_model,
-    persist_directory="chroma_db",
-)
+# # creating a db 
+# vectorstore=Chroma.from_documents(
+#     documents=chunks,
+#     embedding=embedding_model,
+#     persist_directory="chroma_db",
+# )
 
